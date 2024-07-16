fe05c452d2492f685c0c7dcae3586e6305da1305
rlaisqls <rlaisqls@gmail.com>
2024-07-16 15:25:47 +0900

    minusBalance 함수 추가

--- src/main/java/com/example/demo/controller/PurchaseController.java
+++ src/main/java/com/example/demo/controller/PurchaseController.java
@@ -1,5 +1,9 @@
 package com.example.demo.controller;
 
+import com.example.demo.controller.dto.request.*;
+import com.example.demo.controller.dto.response.*;
+import com.example.demo.domain.*;
+import com.example.demo.exceptions.*;
 import lombok.*;
 import org.springframework.security.crypto.password.*;
 import org.springframework.web.bind.annotation.*;
@@ -9,4 +13,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/purchase")
 public class PurchaseController {
 
+    private final BalanceRepository balanceRepository;
+
+    void minusBalance(Long value) {
+        Balance balance = balanceRepository.findById(1L)
+            .orElseThrow(() -> BalanceNotFoundException.EXCEPTION);
+
+        balance.setBalance(balance.getBalance() - value);
+        balanceRepository.save(balance);
+    }
 }
